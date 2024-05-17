cbe12e3b1f78c3c35042c38480dafe5511dfb15f
ikimaka <ikimaka@yandex.ru>
2024-05-17 13:38:55 +0300

    Lesson12: Realize RecyclerView without dataSetChanged by DiffUtils

--- app/src/main/java/com/ikimaka/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/ikimaka/shoppinglist/presentation/ShopListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.ikimaka.shoppinglist.R
 import com.ikimaka.shoppinglist.domain.ShopItem
@@ -15,14 +16,16 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
     var count = 0
     var shopList = listOf<ShopItem>()
         set(value) {
+            val callback = ShopListDiffCallback(shopList, value)
+            val diffResult = DiffUtil.calculateDiff(callback)
+            diffResult.dispatchUpdatesTo(this)
             field = value
-            notifyDataSetChanged()
         }
     var onShopItemLongClickListener: ((ShopItem) -> Unit)? = null
     var onShopItemClickListener: ((ShopItem) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        Log.d("ShopListAdapterTest", "onCreateViewHolder, count ${++count}")
+
         val layout = when (viewType) {
             VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
             VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
@@ -37,7 +40,7 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
     }
 
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
-
+        Log.d("ShopListAdapterTest", "onBindViewHolder, count ${++count}")
         val shopItem = shopList[position]
 
         holder.tvName.text = shopItem.name
