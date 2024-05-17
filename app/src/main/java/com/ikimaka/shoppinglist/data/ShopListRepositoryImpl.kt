e4b275897fbf78b955853a01ff38264a958d1988
ikimaka <ikimaka@yandex.ru>
2024-05-17 07:18:24 +0300

    Lesson9: Bug RecyclerView with different viewTypes, add onViewRecycled

--- app/src/main/java/com/ikimaka/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/ikimaka/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.ikimaka.shoppinglist.domain.ShopItem
 import com.ikimaka.shoppinglist.domain.ShopListRepository
+import kotlin.random.Random
 
 object ShopListRepositoryImpl: ShopListRepository {
 
@@ -13,7 +14,7 @@ object ShopListRepositoryImpl: ShopListRepository {
 
     init {
         for (i in 0 until 20) {
-            val item = ShopItem("Name $i", i, true)
+            val item = ShopItem("Name $i", i, Random.nextBoolean())
             addShopItem(item)
         }
     }
