7441481bfcfa3eee2b90d4834eef6f9ea4a07009
ikimaka <ikimaka@yandex.ru>
2024-05-18 07:26:37 +0300

    Lesson16: Add LiveDates for errors, shopItem and shouldCloseScreen

--- app/src/main/java/com/ikimaka/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/ikimaka/shoppinglist/presentation/ShopItemActivity.kt
@@ -5,8 +5,14 @@ import android.os.Bundle
 import com.ikimaka.shoppinglist.R
 
 class ShopItemActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ShopItemViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+
+
+
+
     }
 }
