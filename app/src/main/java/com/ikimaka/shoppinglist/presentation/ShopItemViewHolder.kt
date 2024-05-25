de356ea87a88d60a4333091644349a7db7310f0b
ikimaka <ikimaka@yandex.ru>
2024-05-25 20:02:48 +0300

    Lesson25: Add DataBinding in Recycler View Adapter

--- app/src/main/java/com/ikimaka/shoppinglist/presentation/ShopItemViewHolder.kt
+++ app/src/main/java/com/ikimaka/shoppinglist/presentation/ShopItemViewHolder.kt
@@ -2,10 +2,9 @@ package com.ikimaka.shoppinglist.presentation
 
 import android.view.View
 import android.widget.TextView
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import com.ikimaka.shoppinglist.R
+import com.ikimaka.shoppinglist.databinding.ItemShopEnabledBinding
 
-class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-    val tvName = view.findViewById<TextView>(R.id.tv_name)
-    val tvCount = view.findViewById<TextView>(R.id.tv_count)
-}
+class ShopItemViewHolder(val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
--- app/src/main/java/com/ikimaka/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/ikimaka/shoppinglist/presentation/ShopListAdapter.kt
@@ -2,8 +2,12 @@ package com.ikimaka.shoppinglist.presentation
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.ListAdapter
 import com.ikimaka.shoppinglist.R
+import com.ikimaka.shoppinglist.databinding.ItemShopDisabledBinding
+import com.ikimaka.shoppinglist.databinding.ItemShopEnabledBinding
 import com.ikimaka.shoppinglist.domain.ShopItem
 
 class ShopListAdapter : ListAdapter<ShopItem, ShopItemViewHolder>(ShopItemDiffCallback()) {
@@ -18,23 +22,37 @@ class ShopListAdapter : ListAdapter<ShopItem, ShopItemViewHolder>(ShopItemDiffCa
             VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
             else -> throw RuntimeException("Unknown view type: $viewType")
         }
-        val view = LayoutInflater.from(parent.context).inflate(layout, parent, false)
-        return ShopItemViewHolder(view)
+        val binding = DataBindingUtil.inflate<ViewDataBinding>(
+            LayoutInflater.from(parent.context),
+            layout,
+            parent,
+            false
+        )
+        return ShopItemViewHolder(binding)
     }
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
         val shopItem = getItem(position)
+        val binding = holder.binding
 
-        holder.tvName.text = shopItem.name
-        holder.tvCount.text = shopItem.count.toString()
-
-        holder.view.setOnLongClickListener {
+        binding.root.setOnLongClickListener {
             onShopItemLongClickListener?.invoke(shopItem)
             true
         }
-
-        holder.view.setOnClickListener {
+        binding.root.setOnClickListener {
             onShopItemClickListener?.invoke(shopItem)
         }
+        when (binding) {
+            is ItemShopEnabledBinding -> {
+                binding.tvName.text = shopItem.name
+                binding.tvCount.text = shopItem.count.toString()
+            }
+            is ItemShopDisabledBinding -> {
+                binding.tvName.text = shopItem.name
+                binding.tvCount.text = shopItem.count.toString()
+            }
+        }
+
+
     }
     override fun getItemViewType(position: Int): Int {
         val item = getItem(position)
