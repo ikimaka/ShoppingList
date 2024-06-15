2438fd4694eb8012771297ed1a657fe104b895cb
ikimaka <ikimaka@yandex.ru>
2024-06-15 09:16:50 +0300

    Delete data

--- app/src/main/java/com/ikimaka/shoppinglist/data/ShopListDao.kt
+++ app/src/main/java/com/ikimaka/shoppinglist/data/ShopListDao.kt
@@ -14,13 +14,20 @@ interface ShopListDao {
     @Query("SELECT * FROM shop_items")
     fun getShopListCursor(): Cursor
 
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addShopItem(shopItemDbModel: ShopItemDbModel)
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun addShopItemSync(shopItemDbModel: ShopItemDbModel)
 
+
     @Query("DELETE FROM shop_items WHERE id=:shopItemId")
     suspend fun deleteShopItem(shopItemId: Int)
+    @Query("DELETE FROM shop_items WHERE id=:shopItemId")
+    fun deleteShopItemSync(shopItemId: Int): Int
+
+
+
     @Query("SELECT * FROM shop_items WHERE id=:shopItemId LIMIT 1")
     suspend fun getShopItem(shopItemId: Int): ShopItemDbModel
 }
--- app/src/main/java/com/ikimaka/shoppinglist/data/ShopListProvider.kt
+++ app/src/main/java/com/ikimaka/shoppinglist/data/ShopListProvider.kt
@@ -72,7 +72,13 @@ class ShopListProvider: ContentProvider() {
     }
 
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
-        TODO("Not yet implemented")
+        when (uriMatcher.match(uri)) {
+            GET_SHOP_ITEMS_QUERY -> {
+                val id = selectionArgs?.get(0)?.toInt() ?: -1
+                return shopListDao.deleteShopItemSync(id)
+            }
+        }
+        return 0
     }
 
     override fun update(
